13b252b83577253ad1b7b906359d1a13bc5d8adc
Jhon Brandon Gutierrez Copa <118786967+BRANDON-YC@users.noreply.github.com>
2025-12-02 08:07:09 -0400

    Change environment to development settings

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,4 @@
 export const environment = {
-  production: true,
-  apiUrl: 'https://perpetual-gentleness-production.up.railway.app/api'
+  production: false,
+  apiUrl: 'http://localhost:8080/api'
 };
